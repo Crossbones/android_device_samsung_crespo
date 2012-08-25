f99b765481a60f4f6d464219323181da83c71068
Mike Wielgosz <dev.mwielgosz@gmail.com>
2012-08-25 14:25:51 -0400

    full_crespo.mk: remove some apps from building
    Change-Id: I3cca94dc2decaca7ec8fc018507d58c38bab2959

--- full_crespo.mk
+++ full_crespo.mk
@@ -22,11 +22,6 @@
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
-# Camera
-PRODUCT_PACKAGES := \
-    Camera \
-    Torch
-
 # Live Wallpapers
 PRODUCT_PACKAGES += \
         LiveWallpapers \
